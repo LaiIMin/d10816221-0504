e43d517a0b8f2eea93822bf1978e68f663a173e8
Oscar <oscar24216587@gmail.com>
2022-06-01 16:57:43 +0800

    fix: set grid & title

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <template>
-  <div></div>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <h1 class="text-center py-10">
+          人員清單
+        </h1> 
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 export default {
