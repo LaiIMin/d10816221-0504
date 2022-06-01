1b5fb7cd2a430ba53c3fe43da41eedab31b94f2c
Oscar <oscar24216587@gmail.com>
2022-06-01 16:54:27 +0800

    fix: init Home.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,12 @@
 <template>
-  <div>
-    <h1>H1</h1>
-    <h2>H2</h2>
-    <h3>h3</h3>
-    <h4>h4</h4>
-    <h5>h5</h5>
-    <h6>h6</h6>
-    <p>
-      p
-        <span>span</span>
-    </p>
-    <small>small</small>
-  </div>
+  <div></div>
 </template>
-
 <script>
 export default {
   data() {
     return {};
-  },
+  }
 };
 </script>
+<style scoped>
+</style>
